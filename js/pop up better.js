24c4789293e5fc3b8aa6e2375a60c1ffe93020c4
Zayd <zaydaly05@gmail.com>
2025-04-14 17:09:43 +0200

    buttons control admin dashboard

--- js/pop up better.js
+++ js/pop up better.js
@@ -1,8 +1,9 @@
-function openModal() {
+//Add User Modal
+function addUseropenModal() {
     document.getElementById("addUserModal").style.display = "block";
   }
 
-  function closeModal() {
+  function addUsercloseModal() {
     document.getElementById("addUserModal").style.display = "none";
   }
 
@@ -14,9 +15,42 @@ function openModal() {
     }
   }
   
+ //Edit User Modal
  
+ function editUseropenModal() {
+  document.getElementById("editUserModal").style.display = "block";
+}
 
+function editUsercloseModal() {
+  document.getElementById("editUserModal").style.display = "none";
+}
 
+// Optional: Close when clicking outside the modal content
 
+window.onclick = function(event) {
+  const modal = document.getElementById("editUserModal");
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+}
+
+ //Delete User Modal
+ 
+ function deleteUseropenModal() {
+  document.getElementById("deleteUserModal").style.display = "block";
+}
+
+function deleteUsercloseModal() {
+  document.getElementById("deleteUserModal").style.display = "none";
+}
+
+// Optional: Close when clicking outside the modal content
+
+window.onclick = function(event) {
+  const modal = document.getElementById("deleteUserModal");
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+}
 
  

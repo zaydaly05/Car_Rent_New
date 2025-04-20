db7947e950c55d43b015f646f5055692544b9e87
omar <omar2300907@miuegypt.edu.eggit config user.namegit config --global user.email omar2300907@miuegypt.edu.eg>
2025-04-20 17:21:32 +0200

    welcome page

--- js/welcome.js
+++ js/welcome.js
@@ -2,15 +2,12 @@ const menu_btn = document.querySelector(".menubtn");
 const navigation = document.querySelector(".navigation");
 
 menu_btn.addEventListener("click", () => {
-   // Only toggle for smaller screens
-        menu_btn.classList.toggle("active");
-        navigation.classList.toggle("active");
-    
+    menu_btn.classList.toggle("active");
+    navigation.classList.toggle("active");
 });
-////slider
-const btns = document.querySelectorAll(".navbtn");
 
-var sliderNav = function (manual){
+// Slider functionality
+var sliderNav = function (manual) {
     btns.forEach((btn) => {
         btn.classList.remove("active");
     });

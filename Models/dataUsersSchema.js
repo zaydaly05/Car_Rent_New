1380c54405e9c658fcb983d1d5affcda8ef96b79
omar <omar2300907@miuegypt.edu.eggit config user.namegit config --global user.email omar2300907@miuegypt.edu.eg>
2025-06-04 23:13:29 +0300

    cc

--- Models/dataUsersSchema.js
+++ Models/dataUsersSchema.js
@@ -2,15 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Registeration = new Schema({
-    FullName: String,
-    Email: String,
-    Address: String,
-    Password: String,
-    type:String,
-    Photo: {
-        data: Buffer,
-        contentType: String
-    }
+    FullName: { type: String, required: true },
+    Email: { type: String, required: true, unique: true },
+    Phone: { type: Number, required: true, unique: true },
+    Password: { type: String, required: true },
+    licence: { type: String, required: true },
+    Registeration: { type: String , required: false }, // Add this line
+    car: { type: String }, // Add this line
+    createdAt: { type: Date, default: Date.now },
 }); 
 
 const User = mongoose.model('User', Registeration);

83145b709977b2b8f720825c5019ae571bd93490
omar hitham <>
2025-06-09 22:54:36 +0300

    updating Views dir

--- routes.js
+++ routes.js
@@ -7,7 +7,7 @@ const User = require('./Models/dataUsersSchema');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'Views', 'views'));
+app.set('views', path.join(__dirname, 'Views'));
 app.set("view engine", "ejs");
 
 app.get('/', (req, res) => {

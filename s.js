fc47b736f8dc5e373f00f4e717650a2aa3461e33
omar <omar2300907@miuegypt.edu.eggit config user.namegit config --global user.email omar2300907@miuegypt.edu.eg>
2025-06-09 21:08:58 +0300

    routes mdified

--- s.js
+++ s.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-const session = require('express-session');
 const app = express();
 const User = require('./Models/dataUsersSchema'); 
 
@@ -11,17 +10,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'Views', 'views'));
 app.set("view engine", "ejs");
 
-app.use(session({
-    secret: 'yourSecretKey',
-    resave: false,
-    saveUninitialized: true
-}));
-
-app.use((req, res, next) => {
-    res.locals.user = req.session.user;
-    next();
-});
-
 app.get('/', (req, res) => {
     res.render('welcome');
 });
@@ -29,30 +17,15 @@ app.post("/register", (req, res) => {
     const { FullName, Email, Phone, Password, licence, car } = req.body;
     const newUser = new User({ FullName, Email, Phone, Password, licence, car });
     newUser.save()
-        .then((user) => {
-            req.session.user = { name: user.FullName, email: user.Email ,Password: user.Password};
-            res.render('login');
+        .then(() => {
+            res.send(`<script>window.top.location.href='/User_Dashboard';</script>`);
         })
         .catch((err) => {
             console.error("Error registering user:", err);
-            res.status(500).send('<span style="color: red;">Email already taken</span>');
+            res.status(500).send('<span style="color: red;">Email already taken</span>'); // Show error in browser
         });
 });
-app.post('/login', async (req, res) => {
-    const { Email, Password } = req.body;
-    try {
-        const user = await User.findOne({ Email, Password });
-        if (user) {
-            req.session.user = { email: user.Email ,Password: user.Password, name: user.FullName };
-            res.send(`<script>window.top.location.href='/User_Dashboard';</script>`);
-        } else {
-            res.status(401).send('<span style="color: red;">Invalid email or password</span>');
-        }
-    } catch (err) {
-        console.error("Login error:", err);
-        res.status(500).send("Server error");
-    }
-});
+
 app.get('/register', (req, res) => {//popup
     res.render('signup');
 });
@@ -64,17 +37,18 @@ app.get("/User_Dashboard", (_req, res) => {
 });
 
 
-app.get('/showroom/luxury', (req, res) => {
+/*app.get('/showroom/luxury', (req, res) => {
     res.render('ShowRoom_Luxury');
-});
+});*/
 
-app.get('/showroom/sedan', (req, res) => {
-    res.render('ShowRoom_Sedan');
-});
+/*app.get('/showroom/sedan', (req, res) => {
+    res.render('ShowRoom_Economy');
+});*/
 
-app.get('/showroom/sports', (req, res) => {
+/*app.get('/showroom/sports', (req, res) => {
     res.render('ShowRoom_Sports');
-});
+});*/
+
 
 const Car = require('./car rent/models/car'); // Adjust path if needed
 
@@ -182,6 +156,7 @@ app.get('/all-cars', async (req, res) => {
         res.status(500).send('Error fetching cars');
     }
 });
+
 
 mongoose.connect("mongodb+srv://omar:123@cars.chdtnqe.mongodb.net/?retryWrites=true&w=majority&appName=cars") 
 .then(() => {

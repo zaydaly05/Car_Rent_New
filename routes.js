3c86c98a267d10a05ccffa840f5c1d96b1f917cb
omar <omar2300907@miuegypt.edu.eggit config user.namegit config --global user.email omar2300907@miuegypt.edu.eg>
2025-06-10 19:28:25 +0300

    Add No Sidebar and Right Sidebar templates for ZeroFour by HTML5 UP

--- routes.js
+++ routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const session = require('express-session');
+const session = require('express-session'); // Only ONCE!
 const mongoose = require('mongoose');
 const path = require('path');
 const app = express();
